a47960ea5a4328b06f6d3c27ae3a3d58fa2c6490
“Alice <aliceletourneur@icloud.com>
2017-04-02 17:10:02 +0100

    Minify HTML, CSS and Javacript

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 // Include gulp
 var gulp = require('gulp');
-// Include plugins
 
+// Include plugins
 var imagemin = require('gulp-imagemin');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-htmlmin');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 
 //Optimize images
 gulp.task('images', function() {
@@ -11,5 +15,29 @@ gulp.task('images', function() {
     .pipe(gulp.dest('dist/img'));
 });
 
+//Minify CSS
+gulp.task('minify-css', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+//Minify HTML
+gulp.task('minify', function() {
+  return gulp.src('src/*.html')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+//Minify Javascript
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
  // Default Task
-gulp.task('default', ['images']);
+gulp.task('default', ['images','minify','minify-css','scripts']);
