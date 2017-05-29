945dba5d591e6604f358bec25d9501da1a4656f3
“Alice <aliceletourneur@icloud.com>
2017-05-29 12:55:16 +0100

    Moving the selection of elements with class .randomPizzaContainer outside the function changePizzaSizes as we only need to select them once

--- dist/views/js/main.js
+++ dist/views/js/main.js
@@ -449,12 +449,15 @@ var resizePizzas = function(size) {
     return dx;
   }
 
+//Create variable to store elements with class .randomPizzaContainer is now outside of the function changePizzaSizes
+var randomPizzaContainer = document.getElementsByClassName('randomPizzaContainer');
+
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < randomPizzaContainer.length; i++) {
+      var dx = determineDx(randomPizzaContainer[i], size);
+      var newwidth = (randomPizzaContainer[i].offsetWidth + dx) + 'px';
+      randomPizzaContainer[i].style.width = newwidth;
     }
   }
 
